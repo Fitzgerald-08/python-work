9b686f6ef4312ea08441bb78ea95bc9fb649e6cc
Fitzgerald-08 <gnulinux.ftw.09@gmail.com>
2024-09-27 21:28:12 -0700

    Adding barebones of new project

---
+++ chapter-six/chapter-six-basics/complex_for_dictionaries.py
@@ -0,0 +1,43 @@
+user_0 = {
+    "username": "fitzgerald",
+    "name": "miles",
+    "email": "hello-1234@gmail.com",
+    "age": 25,
+    "location": "copenhagen",
+    "occupation": "student",
+
+}
+
+user_1 = {
+    "username": "ell3n",
+    "name": "ripley",
+    "email": "alien-hunter.60@protonmail.com",
+    "age": 33,
+    "location": "italy",
+    "occupation": "hacker",
+
+}
+
+user_2 = {
+    "username": "your mother",
+    "name": "tyler",
+    "email": "fuck_0ff.4598@gmail.com",
+    "age": 17,
+    "location": "colombia",
+    "occupation": "student",
+
+}
+
+user_3 = {
+    "username": "soul eater",
+    "name": "tom",
+    "email": "toxicrelationship.09@gmail.com",
+    "age": 30,
+    "location": "spain",
+    "occupation": "help desk",
+
+}
+
+
+user_list = [user_0, user_1, user_2, user_3]
+
